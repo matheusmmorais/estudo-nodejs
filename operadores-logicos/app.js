d834abd084f511c188ba7af962e9a5e1304c530e
matheusmmorais <matheus_mmorais@hotmail.com>
2022-02-11 11:46:14 -0300

    Create app.js

---
+++ operadores-logicos/app.js
@@ -0,0 +1,33 @@
+const { read } = require("fs");
+
+const readLine = require("readline").createInterface ({
+    input: process.stdin,
+    output: process.stdout
+})
+
+console.log("este programa vai checar se você é maior de 18 e tem habilitação para saber se pode entrar no kart");
+console.log("alem das suas verificacoes, precisamos verificar se você esta na lista de presença do horario");
+
+readLine.question("qual o ano do seu nascimento: ", ano => {
+    if (ano > 2004){
+        console.log("Você não tem 18 anos? (Sim/Não)");
+    }else{
+        readLine.question("Você possue habilitação? (Sim/Nao)", habilitacao => {
+           if (!(habilitacao.toUpperCase() === "SIM")) {
+                console.log("Você não possue habilitação para entrar no kart");
+           } else {
+               readLine.question ("Qual seu nome: ", nome => {
+                   switch(nome){
+                        case 'douglas':
+                           console.log("bem vindo ao kart Douglas");
+                        case 'rafael':
+                            console.log("bem vindo ao kart Rafael");
+                        default: 
+                            console.log("Usuario não identificado");
+                   }
+               })
+
+           }
+        })
+    }
+})
