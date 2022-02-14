c283c329a91d7f9987e767b6b604d8a84c13966f
matheusmmorais <matheus_mmorais@hotmail.com>
2022-02-14 09:57:06 -0300

    Update app.js

--- estrutura-de-repeticao/app.js
+++ estrutura-de-repeticao/app.js
@@ -1,1 +1,8 @@
 const listaDeArgumentos = process.argv.slice(2);
+console.log(listaDeArgumentos);
+
+console.log("for ----------------------------- rodando ----------");
+for(let controladorFor = 0; controladorFor < listaDeArgumentos.lenght; controladorFor++){
+    console.log(`posicao ${controladorFor} nome ${listaDeArgumentos[controladorFor]}`);
+    console.log(listaDeArgumentos);
+};
